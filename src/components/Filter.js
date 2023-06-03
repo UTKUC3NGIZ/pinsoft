08c6f6b2cfaca18bc3d1fb11d5efca7295093d03
Utku Cengiz <cekicengiz01@gmail.com>
2023-06-04 02:13:24 +0300

    filter section outlines were created

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,7 +3,26 @@ import React from "react";
 function Filter() {
   return (
     <>
-      <div></div>
+      <div className="flex flex-col w-1/6 border-r-2">
+        <div className="text-xl p-8 hover:bg-slate-900 flex justify-between items-center">
+          <h3 className="text-white">jewelery</h3>
+          <span className="text-white border-b-2  flex items-center justify-center text-base ">
+            2
+          </span>
+        </div>
+        <div className="text-xl p-8 hover:bg-slate-900 flex justify-between items-center ">
+          <h3 className="text-white">jewelery</h3>
+          <span className="text-white border-b-2  flex items-center justify-center text-base">
+            2
+          </span>
+        </div>
+        <div className="text-xl p-8 hover:bg-slate-900 flex justify-between items-center">
+          <h3 className="text-white ">jewelery</h3>
+          <span className="text-white border-b-2  flex items-center justify-center text-base">
+            2
+          </span>
+        </div>
+      </div>
     </>
   );
 }
