1326215593847c40e0072508760671e63ff5bc7a
Utku Cengiz <cekicengiz01@gmail.com>
2023-06-04 11:53:01 +0300

    products section json

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import React, { useEffect, useState } from "react";
+
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Filter from "./components/Filter";
 import Products from "./components/Products";
+import jsonData from "./data/products-list.json";
+
 function App() {
+  const [products, setProducts] = useState(jsonData);
+
   return (
     <div className="h-full bg-slate-950">
       <Navbar />
       <div className="flex ">
         <Filter />
-        <Products />
+        <Products products={products} />
       </div>
     </div>
   );
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,37 +1,51 @@
 import React from "react";
 import ReactStars from "react-rating-stars-component";
 
-function Products() {
+function Products(props) {
   return (
     <>
       <div className="grid grid-cols-4 w-5/6 gap-10 m-8 ">
-        <div className="flex flex-col bg-white p-6">
-          <img
-            src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-            alt=""
-          />
-          <h4 className="pt-6 border-b-2 border-black pb-3 text-center text-xl">
-            Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
-          </h4>
-          <span className="pt-3 text-center text-xl">109.95 $</span>
-          <div className="flex flex-col">
-            <button className=" border-2 border-black py-3 my-3 text-xl">
-              Add to cart
-            </button>
-            <div className="flex items-center">
-              <ReactStars
-                count={5}
-                value={3.5}
-                size={28}
-                activeColor="#ffd700"
-                color="#000"
-                edit={false}
-                isHalf={true}
+        {props.products.map((product) => (
+          <div
+            className="flex flex-col justify-between bg-white p-6"
+            key={product.id}
+          >
+            <div className="h-full flex items-center">
+              <img
+                src={product.image}
+                alt={product.title}
+                className="w-full aspect-auto"
               />
-              <span className="text-sm">(124) </span>
+            </div>
+            <div className="flex flex-col">
+              <h4 className="pt-6 border-b-2 border-black pb-3 text-center text-xl">
+                {product.title}
+              </h4>
+              <span className="pt-3 text-center text-xl">
+                {product.price} $
+              </span>
+              <div className="flex flex-col">
+                <button className=" border-2 border-black py-3 my-3 text-xl">
+                  Add to cart
+                </button>
+                <div className="flex items-center">
+                  <ReactStars
+                    count={5}
+                    value={product.rating.rate}
+                    size={28}
+                    activeColor="#ffd700"
+                    color="#000"
+                    edit={false}
+                    isHalf={true}
+                  />
+                  <span className="text-sm pl-1">
+                    ({product.rating.count}){" "}
+                  </span>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </>
   );
